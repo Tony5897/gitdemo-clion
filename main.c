e76fd63d6eabbd9a099c0cf39178d7b5da8f5408
tonym <tony.martinez5897@gmail.com>
2022-10-20 13:09:10 -0700

    CLion demo format for integers

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
+/* This program adds two integer values and displays the results */
+
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(void)
+{
+    int sum;
+
+    sum = 50 + 25;
+    printf("The sum of 50 and 25 is %i\n", sum);
+
     return 0;
 }
